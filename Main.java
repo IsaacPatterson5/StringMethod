ac1daaf3a68031531ffdfec7be87efa40f97597f
Isaac Patterson <85522516+IsaacPatterson5@users.noreply.github.com>
2021-12-09 03:37:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+/**
+ *  This program demonstrates
+ *  length String methods.
+ */
+public class Main
+{
+   public static void main(String[] args)
+   {
+      String message;    // To hold a string
+      int n;            // To hold length of String
+
+      message = "The Java Tutorials";
+      n = message.length();
+      System.out.println("The length of message : " + n);
+      System.out.println("");
+
+      message = "The Java Tutorials";
+      System.out.println(message.toLowerCase());
+      System.out.println(message.toUpperCase());
+      System.out.println(message.concat(" For Beginners"));
+      System.out.println(message.substring(4));
+      System.out.println(message.substring(4, 8));
+   }
+}
